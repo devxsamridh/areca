6d67e6feed1196d22d3f68c5343c40d7cd9e7c77
Samridh Shaurya Singh <168576137+devxsamridh@users.noreply.github.com>
2026-04-05 15:39:32 +0530

    Create dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,17 @@
+class MyDataset(Dataset):
+    def __init__(self, paths, labels, transform=None):
+        self.paths = paths
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.paths[idx]).convert("RGB")
+        label = self.labels[idx]
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, label
